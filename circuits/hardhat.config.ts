aedfcb8afd8e295862ed28fadbddbf67bc5cba59
Bernardo Vieira <19441097+obernardovieira@users.noreply.github.com>
2024-01-26 17:40:59 +0000

    circuits to scroll chain

--- circuits/hardhat.config.ts
+++ circuits/hardhat.config.ts
@@ -1,9 +1,21 @@
+import * as dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-viem";
+import "@nomicfoundation/hardhat-verify";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
+	defaultNetwork: "scrollSepolia",
+	networks: {
+		scrollSepolia: {
+			url: process.env.SCROLL_TESTNET_URL || "",
+			accounts:
+				process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+		},
+	},
 	solidity: {
-		version: '0.8.19',
+		version: "0.8.20",
 		settings: {
 			optimizer: { enabled: true, runs: 5000 },
 		},
@@ -11,6 +23,24 @@ const config: HardhatUserConfig = {
 	paths: {
 		sources: "./circuits/contract/circuits",
 	},
+	etherscan: {
+		apiKey: {
+			scrollSepolia: process.env.SCROLLSCAN_API_KEY!,
+		},
+		customChains: [
+			{
+				network: "scrollSepolia",
+				chainId: 534351,
+				urls: {
+					apiURL: "https://api-sepolia.scrollscan.com/api",
+					browserURL: "https://api-sepolia.scrollscan.com/",
+				},
+			},
+		],
+	},
+	sourcify: {
+		enabled: false
+	}
 };
 
 export default config;
